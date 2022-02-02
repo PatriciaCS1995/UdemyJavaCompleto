11bf568ab8bfef0a1b2a3cfc717274436a01cd91
Paty <78480425+PatriciaCS1995@users.noreply.github.com>
2022-02-01 21:06:51 -0300

    Reorganizando atividades.
    add atividade aula 14, trabalhadores comuns e terceirizados.

---
+++ AulaQuatorze/Aplication/MainEmployee.java
@@ -0,0 +1,53 @@
+package Aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class MainEmployee {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> listEmployee = new ArrayList<>();
+		
+		System.out.print("Enter the number of employee: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++){
+			System.out.println("Employee #" + i + "data: ");
+			System.out.println("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Hours: ");
+			int hours = sc.nextInt();
+			System.out.println("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			if(ch == 'y') {
+				System.out.println("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				listEmployee.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+			}
+			else {
+				listEmployee.add(new Employee(name, hours, valuePerHour));
+			}
+			
+		}
+		
+		System.out.println();
+		System.out.println("PAYMENTS: ");
+		for (Employee emp : listEmployee) {
+			System.out.println(emp.getName() + " - $ " + String.format("%.2f",emp.payment()));
+		}
+		sc.close();
+		
+	}
+
+}
